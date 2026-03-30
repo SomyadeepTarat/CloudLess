47c8e95ef5b3afc570b2d1be24cd1878d69983db
SomyadeepTarat <sayantarat@gmail.com>
2026-03-30 16:02:22 +0530

    Backend

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const { initWebSocket } = require('./websocket/socketserver');
 const { removeDeadNodes } = require('./services/nodeservice');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const server = http.createServer(app);
 
